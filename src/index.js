238c6bfa159edf29cf837840ce2e7824ccb0066f
Evgeniy <95440463+MEvgeniy21@users.noreply.github.com>
2022-06-30 02:17:05 +0300

    general task

--- src/index.js
+++ src/index.js
@@ -1,1 +1,80 @@
-// import { fetchCountries } from './js/fetchPixabay';
+import { fetchImage, resetPage, page, per_page } from './js/fetchPixabay';
+import { refs } from './js/refs';
+import { markupImage } from './js/markup';
+
+const clsHidden = 'is-hidden';
+let query = '';
+let total = 0;
+console.log(refs);
+
+refs.form.addEventListener('submit', onClickSubmit);
+refs.btnLoadMore.addEventListener('click', onClickLoadMore);
+
+function onClickSubmit(event) {
+  event.preventDefault();
+  clearMarkup();
+  hideBtn(refs.btnLoadMore);
+  resetPage();
+  query = refs.form.elements.searchQuery.value.trim().toLowerCase();
+
+  if (query.length < 3) {
+    console.log('few characters to search');
+    return;
+  }
+
+  fetchImage(query)
+    .then(materials => {
+      total = parseInt(materials.total, 10);
+      console.log(materials.total);
+      if (!materials.hits.length) {
+        console.log(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+        return;
+      }
+      materials.hits.forEach(element => {
+        refs.gallery.insertAdjacentHTML('beforeend', markupImage(element));
+      });
+      if (total > per_page) {
+        showBtn(refs.btnLoadMore);
+      }
+      refs.form.reset();
+    })
+    .catch(console.log);
+}
+
+function onClickLoadMore() {
+  console.log('Load more');
+  const checkPage = Math.ceil(total / per_page);
+
+  fetchImage(query)
+    .then(materials => {
+      console.log(materials.total);
+      materials.hits.forEach(element => {
+        refs.gallery.insertAdjacentHTML('beforeend', markupImage(element));
+      });
+    })
+    .catch(console.log);
+
+  if (checkPage === page) {
+    console.log('Last page');
+    hideBtn(refs.btnLoadMore);
+    return;
+  }
+}
+
+function clearMarkup() {
+  refs.gallery.innerHTML = '';
+}
+
+function showBtn(btn) {
+  if (btn.classList.contains(clsHidden)) {
+    btn.classList.remove(clsHidden);
+  }
+}
+
+function hideBtn(btn) {
+  if (!btn.classList.contains(clsHidden)) {
+    btn.classList.add(clsHidden);
+  }
+}
--- src/js/fetchPixabay.js
+++ src/js/fetchPixabay.js
@@ -2,3 +2,28 @@ import axios from 'axios';
 // axios.get('/users').then(res => {
 //   console.log(res.data);
 // });
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+
+export const per_page = 40;
+export let page = 1;
+
+export function fetchImage(q) {
+  const searchParams = new URLSearchParams({
+    key: '28350723-a2da361fc29768379678747a9',
+    q,
+    page,
+    per_page,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+  });
+
+  return axios.get(`?${searchParams}`).then(response => {
+    page += 1;
+    return response.data;
+  });
+}
+
+export function resetPage() {
+  page = 1;
+}
